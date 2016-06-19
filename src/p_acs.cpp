c4f59bab96cb6af993c70acf5e3caad4bca8d259
Edoardo Prezioso <edo88@email.it>
2016-06-20 01:42:20 +0200

    - Add support for Skulltag ACS IsNetworkGame.
    Once known as PlayerOnTeam, then it became IsMultiplayer, but Skulltag code ignored emulated multiplayer, hence the new and clearer name.
    
    (cherry picked from commit 6384e81d0f135a2c292ac3e874f6fe26093f45b1)

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -8127,6 +8127,10 @@ scriptwait:
 			break;
 
 // [BC] Start ST PCD's
+		case PCD_ISNETWORKGAME:
+			PushToStack(netgame);
+			break;
+
 		case PCD_PLAYERTEAM:
 			if ( activator && activator->player )
 				PushToStack( activator->player->userinfo.GetTeam() );
