083f170cddea1c06167f4d2232e4744cb23770ca
Edoardo Prezioso <edo88@email.it>
2016-06-19 22:56:35 +0200

    - Added support for old Skulltag ACS PlayerTeam.
    (cherry picked from commit 2f6c98ead3029a5290455f2c7d6b25d994bad828)

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -8127,6 +8127,13 @@ scriptwait:
 			break;
 
 // [BC] Start ST PCD's
+		case PCD_PLAYERTEAM:
+			if ( activator && activator->player )
+				PushToStack( activator->player->userinfo.GetTeam() );
+			else
+				PushToStack( 0 );
+			break;
+
 		case PCD_PLAYERHEALTH:
 			if (activator)
 				PushToStack (activator->health);
