dc7237ef215ebf2121a52ee3449c9bdc5ed0fa89
raa-eruanna <eruanna@drdteam.org>
2016-10-06 20:01:20 -0400

    - Backported blood_fade_scalar from Skulltag - Added new pickup_fade_scalar which works the same way for pickups - Default for blood_fade_scalar is 1.0 instead of 0.5 from Skulltag.
    (cherry picked from commit 02f66fa34e2bdb63ca1ba740c5b35b7388550120)
    
    # Conflicts:
    #	wadsrc/static/menudef.txt
    
    # Conflicts:
    #	wadsrc/static/menudef.z

--- src/v_blend.cpp
+++ src/v_blend.cpp
@@ -54,7 +54,8 @@
 #include "farchive.h"
 #include "a_hexenglobal.h"
 
-
+CVAR( Float, blood_fade_scalar, 1.0f, CVAR_ARCHIVE )	// [SP] Pulled from Skulltag - changed default from 0.5 to 1.0
+CVAR( Float, pickup_fade_scalar, 1.0f, CVAR_ARCHIVE )	// [SP] Uses same logic as blood_fade_scalar except for pickups
 
 // [RH] Amount of red flash for up to 114 damage points. Calculated by hand
 //		using a logarithmic scale and my trusty HP48G.
@@ -115,6 +116,9 @@ void V_AddPlayerBlend (player_t *CPlayer, float blend[4], float maxinvalpha, int
 	if (CPlayer->bonuscount)
 	{
 		cnt = CPlayer->bonuscount << 3;
+
+		// [SP] Allow player to tone down intensity of pickup flash.
+		cnt = (int)( cnt * pickup_fade_scalar );
 		
 		V_AddBlend (RPART(gameinfo.pickupcolor)/255.f, GPART(gameinfo.pickupcolor)/255.f, 
 					BPART(gameinfo.pickupcolor)/255.f, cnt > 128 ? 0.5f : cnt / 255.f, blend);
@@ -126,7 +130,10 @@ void V_AddPlayerBlend (player_t *CPlayer, float blend[4], float maxinvalpha, int
 	if (painFlash.a != 0)
 	{
 		cnt = DamageToAlpha[MIN (113, CPlayer->damagecount * painFlash.a / 255)];
-			
+
+		// [BC] Allow users to tone down the intensity of the blood on the screen.
+		cnt = (int)( cnt * blood_fade_scalar );
+
 		if (cnt)
 		{
 			if (cnt > maxpainblend)
