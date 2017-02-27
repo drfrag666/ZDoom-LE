d743282bfe38e928a3bbe7053daa2c389423a2e4
Christoph Oelckers <coelckers@users.noreply.github.com>
2017-02-27 10:37:47 +0100

    - added an option to add elements to an existing options menu.
    Note that for list menus this does not exist because they require complete setup to work as intended.
    
    (cherry picked from commit 0c660ff29ca9f2915f9aaea31edaed374c7fd7e5)
    
    - fixed compilation. (drfrag)

--- src/menu/menudef.cpp
+++ src/menu/menudef.cpp
@@ -941,6 +941,25 @@ static void ParseOptionMenu(FScanner &sc)
 //
 //=============================================================================
 
+static void ParseAddOptionMenu(FScanner &sc)
+{
+	sc.MustGetString();
+
+	FMenuDescriptor **pOld = MenuDescriptors.CheckKey(sc.String);
+	if (pOld == NULL || *pOld == NULL || !(*pOld)->mType == MDESC_OptionsMenu)
+	{
+		sc.ScriptError("%s is not an option menu that can be extended", sc.String);
+	}
+	ParseOptionMenuBody(sc, (FOptionMenuDescriptor*)(*pOld));
+}
+
+
+//=============================================================================
+//
+//
+//
+//=============================================================================
+
 void M_ParseMenuDefs()
 {
 	int lump, lastlump = 0;
@@ -995,6 +1014,10 @@ void M_ParseMenuDefs()
 			else if (sc.Compare("OPTIONMENU"))
 			{
 				ParseOptionMenu(sc);
+			}
+			else if (sc.Compare("ADDOPTIONMENU"))
+			{
+				ParseAddOptionMenu(sc);
 			}
 			else if (sc.Compare("DEFAULTOPTIONMENU"))
 			{
