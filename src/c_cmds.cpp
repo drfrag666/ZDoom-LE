04e0ee5000e77cefe04e57289ddda9b1135ee037
drfrag <drfrag666@hotmail.com>
2017-03-21 13:01:29 +0100

    - Added compilation time to the gameversion cmd (from ZDoom CL).

--- src/c_cmds.cpp
+++ src/c_cmds.cpp
@@ -423,7 +423,7 @@ CCMD (take)
 CCMD (gameversion)
 {
 	//Printf ("%s @ %s\nCommit %s\n", GetVersionString(), GetGitTime(), GetGitHash());
-	Printf ("%s : MAR 2017\n", GetVersionString());
+	Printf ("%s : " __DATE__ "\n", GetVersionString());
 }
 
 CCMD (print)
