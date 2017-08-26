f8214ffe5c2107982ffd290430bfe9db6d34294a
drfrag <drfrag666@hotmail.com>
2017-08-26 12:10:04 +0200

    - Minor changes to vid_forceddraw.

--- src/win32/win32video.cpp
+++ src/win32/win32video.cpp
@@ -129,7 +129,20 @@ IDirect3DDevice9 *D3Device;
 #endif
 HANDLE FPSLimitEvent;
 
-CVAR (Bool, vid_forceddraw, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+int currentbackend = 0;
+
+CUSTOM_CVAR (Bool, vid_forceddraw, false, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+{
+	if (self != currentbackend)
+	{
+		if (self)
+			Printf("Forcing DirectDraw. ");
+		else
+			Printf("Enabling Direct3D if available. ");
+		Printf("You must restart " GAMENAME " to switch the renderer.\n");
+	}
+	currentbackend=self;
+}
 CVAR (Int, vid_adapter, 1, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CUSTOM_CVAR (Int, vid_maxfps, 200, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 {
