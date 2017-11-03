johncurley/gzdoom

- fixed z-offset handling of A_SkelMissile.
I can't believe I missed this for more than 10 years, considering that A_CustomMissile explicitly implements this case:
It makes a crucial difference whether P_SpawnMissileZ is used or the actual z-position is temporarily changed.
Reverted this function to the position changing method of the original.

src/g_doom/a_revenant.cpp
@@ -28,8 +28,9 @@ DEFINE_ACTION_FUNCTION(AActor, A_SkelMissile)
 		return 0;
 				
 	A_FaceTarget (self);
-	missile = P_SpawnMissileZ (self, self->Z() + 48*FRACUNIT,
-		self->target, PClass::FindActor("RevenantTracer"));
+	self->AddZ(16*FRACUNIT);
+	missile = P_SpawnMissile (self, self->target, PClass::FindActor("RevenantTracer"));
+	self->AddZ(-16*FRACUNIT);
 
 	if (missile != NULL)
 	{

drfrag666/ZDoom-LE

(cherry picked from commit f1d2c7755a233d76a8a2f8ebd5cdbaa9ffbebc96)
# Conflicts:
#	src/g_doom/a_revenant.cpp

src/g_doom/a_revenant.cpp
@@ -26,8 +26,9 @@ DEFINE_ACTION_FUNCTION(AActor, A_SkelMissile)
 		return;
 				
 	A_FaceTarget (self);
-	missile = P_SpawnMissileZ (self, self->Z() + 48*FRACUNIT,
-		self->target, PClass::FindClass("RevenantTracer"));
+	self->AddZ(16*FRACUNIT);
+	missile = P_SpawnMissile (self, self->target, PClass::FindClass("RevenantTracer"));
+	self->AddZ(-16*FRACUNIT);
 
 	if (missile != NULL)
 	{