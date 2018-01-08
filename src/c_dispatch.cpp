9e9644b99910f53b6ce45c8d5eb14a609a9d34f4
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-01-08 10:41:31 +0200

    Added propagation of unsafe execution context to waiting command
    Thanks Edward-san for pointing this out
    
    (cherry picked from commit b008426ed7c6f24a8033111b24147b6822a7bd08)
    
    # Conflicts:
    #	src/c_dispatch.cpp

--- src/c_dispatch.cpp
+++ src/c_dispatch.cpp
@@ -63,7 +63,7 @@ class DWaitingCommand : public DThinker
 {
 	DECLARE_CLASS (DWaitingCommand, DThinker)
 public:
-	DWaitingCommand (const char *cmd, int tics);
+	DWaitingCommand (const char *cmd, int tics, bool unsafe);
 	~DWaitingCommand ();
 	void Serialize (FArchive &arc);
 	void Tick ();
@@ -73,6 +73,7 @@ private:
 
 	char *Command;
 	int TicsLeft;
+	bool IsUnsafe;
 };
 
 class DStoredCommand : public DThinker
@@ -200,12 +201,14 @@ DWaitingCommand::DWaitingCommand ()
 {
 	Command = NULL;
 	TicsLeft = 1;
+	IsUnsafe = false;
 }
 
-DWaitingCommand::DWaitingCommand (const char *cmd, int tics)
+DWaitingCommand::DWaitingCommand (const char *cmd, int tics, bool unsafe)
 {
 	Command = copystring (cmd);
 	TicsLeft = tics+1;
+	IsUnsafe = unsafe;
 }
 
 DWaitingCommand::~DWaitingCommand ()
@@ -220,7 +223,10 @@ void DWaitingCommand::Tick ()
 {
 	if (--TicsLeft == 0)
 	{
+		const bool wasUnsafe = UnsafeExecutionContext;
+		UnsafeExecutionContext = IsUnsafe;
 		AddCommandString (Command);
+		UnsafeExecutionContext = wasUnsafe;
 		Destroy ();
 	}
 }
@@ -725,7 +731,7 @@ void AddCommandString (char *cmd, int keynum)
 						  // Note that deferred commands lose track of which key
 						  // (if any) they were pressed from.
 							*brkpt = ';';
-							new DWaitingCommand (brkpt, tics);
+							new DWaitingCommand (brkpt, tics, UnsafeExecutionContext);
 						}
 						return;
 					}
