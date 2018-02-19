64d03b30fa5aca987962571770f37a09794b502c
alexey.lysiuk <alexey.lysiuk@gmail.com>
2018-02-19 16:57:52 +0200

    Set target for blood actor with PUFFGETSOWNER flag spawned by crusher
    https://forum.zdoom.org/viewtopic.php?t=59447
    (cherry picked from commit a3236aa7ef36390f7d3f25cd4c1843c8b5ab1c4a)

--- src/p_map.cpp
+++ src/p_map.cpp
@@ -5167,6 +5167,11 @@ void P_DoCrunch(AActor *thing, FChangePosition *cpos)
 						mo->Translation = TRANSLATION(TRANSLATION_Blood, bloodcolor.a);
 					}
 
+					if (mo->flags5 & MF5_PUFFGETSOWNER)
+					{
+						mo->target = thing;
+					}
+
 					if (!(cl_bloodtype <= 1)) mo->renderflags |= RF_INVISIBLE;
 				}
 
