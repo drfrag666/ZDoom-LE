0dcf9184957179a3194e14e8c46afe648bffcc23
drfrag <drfrag666@hotmail.com>
2018-03-28 13:01:36 +0200

    - Adjusted savegame comment size for Heretic and Hexen.

--- src/menu/loadsavemenu.cpp
+++ src/menu/loadsavemenu.cpp
@@ -594,6 +594,7 @@ void DLoadSaveMenu::Drawer ()
 	if (SaveComment != NULL)
 	{
 		int maxlines = screen->GetHeight()>400?10:screen->GetHeight()>240?7:screen->GetHeight()>200?8:5;
+		if (SmallFont->GetHeight() > 9) maxlines--; // not Doom
 		// I'm not sure why SaveComment would go NULL in this loop, but I got
 		// a crash report where it was NULL when i reached 1, so now I check
 		// for that.
